58cb819c84e716c78fbe6a5fec36aff2b85dcff7
Adam Nirnberg <nirnberga@spseplzen.cz>
2023-04-18 08:16:39 +0200

    pripojena db k clanku

---
+++ clanek.php
@@ -0,0 +1,27 @@
+<?php
+$clanekId = $_GET["id"];
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "travelblog";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    
+</body>
+</html>
