c025d75b79eb25cb5b4deb3e600cf7e48d171960
Jakub Šulc <sulcj@spseplzen.cz>
2023-04-24 18:39:40 +0200

    Upraven styl pravé části stránky a pozadí u článků

--- clanek.php
+++ clanek.php
@@ -8,15 +8,38 @@ $clanek = new Clanek($_GET["id"]);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $clanek->get_title() ?></title>
+    <title>TravelBlog Prestige - <?php echo $clanek->get_title() ?></title>
+    <link rel="stylesheet" href="./css/style.css">
+    <script src="https://kit.fontawesome.com/c4254e24a8.js" crossorigin="anonymous"></script>
 </head>
 <body>
+<div class="header">
+        <!-- Navigační menu -->
+        <div class="side-nav">
+            <a href="./index.php" class="logo">
+                <img src="./img/logo.png" alt="logo" class="logo-img">
+                <img src="./img/logo-mobile.png" alt="logo" class="logo-mobile">
+            </a>
+            <ul class="nav-links">
+                <li><a href="./index.php"><i class="fa-solid fa-house-user"></i><p>Hlavní stránka</p></a></li>
+                <li><a href="./napsatClanek.html"><i class="fa-solid fa-pencil"></i><p>Napsat článek</p></a></li>
+                <li><a href="#"><i class="fa-solid fa-code"></i><p>Teprve bude</p></a></li>
+                <li><a href="#"><i class="fa-solid fa-code"></i><p>Teprve bude</p></a></li>
+                <li><a href="#"><i class="fa-solid fa-code"></i><p>Teprve bude</p></a></li>
+                <li><a href="./nastaveni.html"><i class="fa-solid fa-gear"></i><p>Nastavení</p></a></li>
+                <div class="active"></div>
+            </ul>
+        </div>
+    </div>
+    <!-- Stránka (pravá strana) -->
+    <div class="clanek_background">
     <?php
-        echo "<h1>".$clanek->get_title()."</h1>";
-        echo "<h2>".$clanek->get_author().", ".$clanek->get_destination().", ".$clanek->get_date()."</h2>";
+        echo "<h2>".$clanek->get_title()."</h2>";
+        echo "<h3>".$clanek->get_author().", ".$clanek->get_destination().", ".$clanek->get_date()."</h3>";
         echo "<p>".$clanek->get_content()."</p>";
         echo "<br>";
         echo $clanek->get_image();
     ?>
+    </div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -7,38 +7,40 @@ require "./ClanekObjekt.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TravelBlog Prestige</title>
+    <title>TravelBlog Prestige - Main Page</title>
     <link rel="stylesheet" href="./css/style.css">
     <script src="https://kit.fontawesome.com/c4254e24a8.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <div class="header">
+        <!-- Navigační menu -->
         <div class="side-nav">
-            <a href="#" class="logo">
+            <a href="./index.php" class="logo">
                 <img src="./img/logo.png" alt="logo" class="logo-img">
                 <img src="./img/logo-mobile.png" alt="logo" class="logo-mobile">
             </a>
             <ul class="nav-links">
-                <li><a href="./index.html"><i class="fa-solid fa-house-user"></i><p>Hlavní stránka</p></a></li>
+                <li><a href="./index.php"><i class="fa-solid fa-house-user"></i><p>Hlavní stránka</p></a></li>
                 <li><a href="./napsatClanek.html"><i class="fa-solid fa-pencil"></i><p>Napsat článek</p></a></li>
-                <li><a href="#"><p>Teprve bude</p></a></li>
-                <li><a href="#"><p>Teprve bude</p></a></li>
-                <li><a href="#"><p>Teprve bude</p></a></li>
+                <li><a href="#"><i class="fa-solid fa-code"></i><p>Teprve bude</p></a></li>
+                <li><a href="#"><i class="fa-solid fa-code"></i><p>Teprve bude</p></a></li>
+                <li><a href="#"><i class="fa-solid fa-code"></i><p>Teprve bude</p></a></li>
                 <li><a href="./nastaveni.html"><i class="fa-solid fa-gear"></i><p>Nastavení</p></a></li>
                 <div class="active"></div>
             </ul>
         </div>
     </div>
-    <div class="nadpis">
-        <h1 class="nadpis-h1"></h1>
-    </div>
-    <div class="clanky">
+    <!-- Stránka (pravá strana) -->
+    <div class="body_right">
         <?php
         for ($i=1; $i <= 3; $i++) { 
+            echo '<div class="clanek_background">';
             $clanek = new Clanek($i);
-            echo "<a href='clanek.php?id=".$i."'>".$clanek->get_title()."</a><br>";
+            echo "<h2>".$clanek->get_title()."</h2>";
+            echo "<a href='clanek.php?id=".$i."'>Zobrazit článek</a><br>";
+            echo '</div>';
         }
         ?>
-        </div>
+    </div> 
 </body>
 </html>
